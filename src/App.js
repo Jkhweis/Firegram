c6967cb08bcc6e92cc9b0c35c06c3b360be54bfa
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-01-12 20:58:42 -0600

    Added modal window

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from './comps/Title';
 import ImageGrid from './comps/ImageGrid';
 import UploadForm from './comps/UploadForm';
+import Modal from './comps/Modal';
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className="App">
       <Title />
-      <ImageGrid />
       <UploadForm />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 }
