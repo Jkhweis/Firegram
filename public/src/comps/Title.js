346353eaec6a67bacd430b8963860715892561c0
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-01-15 22:45:12 -0600

    Updated Styling

--- public/src/comps/Title.js
+++ public/src/comps/Title.js
@@ -4,7 +4,7 @@ const Title = () => {
   return (
     <div className="title">
       <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
+      <h2>Your Pictures Testing Deploy</h2>
     </div>
   );
 };
